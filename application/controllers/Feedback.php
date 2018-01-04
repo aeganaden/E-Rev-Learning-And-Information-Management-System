2b8b257a4883030bbd63bb121d195840f3bd0c2d
markdenverPH <markdenverbabaran@gmail.com>
2018-01-04 17:50:30 +0800

    commit - mark
    feedback again

--- application/controllers/Feedback.php
+++ application/controllers/Feedback.php
@@ -3,9 +3,7 @@
 class Feedback extends CI_Controller {
 
     public function index() {
-        $this->load->view('includes/header');
         $this->load->view('feedback/feedback_main');
-        $this->load->view('includes/footer');
     }
 
 }
