206c4d7b4e17e9a3701220b19848f92107510b04
Moy Ganaden <angelojrganaden@gmail.com>
2018-01-11 16:44:53 +0800

    commit - ganaden
    added home-navbar

--- application/views/home-activity.php
+++ application/views/home-activity.php
@@ -1,23 +1,6 @@
-<!--====================================
-=            Navigation Top            =
-=====================================-->
 
-<div class="row valign-wrapper">
-	<div class="col s4 ">
 
-		<a href="#" style="padding-left: 2%" data-activates="slide-out" class="color-black button-collapse valign-wrapper">
-			<i class="material-icons small">menu</i> MENU
-		</a>
-	</div>
-	<div class="col s4">
-		<center>
-			<img src="<?=base_url() ?>assets/img/feu-header.png" style="width: 40vh; margin-top: 2%;">
-		</center>
-	</div>
-	<div class="col s4"></div>
-</div>
-
-<!--====  End of Navigation Top  ====<--></-->
+<?php $this->load->view('includes/home-navbar'); ?>
 <?php $this->load->view('includes/home-sidenav'); ?>
 
 <div class="row">
@@ -54,7 +37,7 @@
 								<p> <?=date("M d, Y | h:i A", $value->activity_date_time)?></p>
 								<p><?=strtoupper($value->activity_venue)?></p>
 							</blockquote>
-							<div class="row" id="div-edit" style="display: none">
+							<div class="row div-edit" style="display: none">
 								<input type="text" class="timepicker" value="Change Time">
 								<input type="text" class="datepicker" value="Change Date">
 							</div>
@@ -63,7 +46,7 @@
 						<div class="col s4">
 
 							<i class="material-icons right color-white tooltipped" data-tooltip="Delete Activity" data-id="<?=$value->activity_id?>" style="cursor: pointer;">delete</i>
-							<i class="tooltipped btn_edit_activity material-icons right color-primary-yellow" data-id="<?=$value->activity_id?>" href="#modal_activity" style="cursor: pointer;" data-tooltip="Change Description">edit</i>
+							<i class="tooltipped btn_edit_activity material-icons right color-primary-yellow" data-id="<?=$value->activity_id?>" href="#modal_activity" style="cursor: pointer;" data-tooltip="Change Activity">edit</i>
 						</div>
 					</div>
 					<h5><?=$value->activity_description?></h5>
@@ -89,7 +72,7 @@
 
 
 <script type="text/javascript">
-	$("#btn_edit_activity").click(function(event) {
-		$("#div-edit").fadeIn();
+	$(".btn_edit_activity").click(function(event) {
+		$(".div-edit").toggle("slow");
 	});
 </script>
