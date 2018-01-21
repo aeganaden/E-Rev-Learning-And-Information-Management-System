c061ce189ff605c7f7bc6469782e53ac0e2c4b56
markdenverPH <markdenverbabaran@gmail.com>
2018-01-21 18:30:00 +0800

    mark
    bla

---
+++ application/controllers/Mobile.php
@@ -0,0 +1,34 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Mobile extends CI_Controller {
+
+    function __construct() {
+        parent::__construct();
+        $this->load->model('Crud_model');
+    }
+
+    public function login() {
+        $_POST['username'] = "mgbabaran";
+        $_POST['pass'] = "mark";
+        $where = array(
+            "username" => $this->input->post("username"),
+            "password" => $this->input->post("pass")
+        );
+
+        if ($result = $this->Crud_model->fetch("student", $where)[0]) {
+
+        } else if ($result = $this->Crud_model->fetch("professor", $where)) {
+
+        } else if ($result = $this->Crud_model->fetch("admin", $where)) {
+
+        }
+//        } else if ($result = $this->Crud_model->fetch("fic", $where)) {
+//
+//        }
+        print_r(json_encode($result));
+        return (json_encode($result));
+    }
+
+}
