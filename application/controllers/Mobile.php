2d41ec58552d594fe29e65485930c09cac9ee493
markdenverPH <markdenverbabaran@gmail.com>
2018-02-08 10:08:16 +0800

    dbase pic update
    bubu

--- application/controllers/Mobile.php
+++ application/controllers/Mobile.php
@@ -19,10 +19,15 @@ class Mobile extends CI_Controller {
 
         if ($result['result'] = $this->Crud_model->fetch_array("student", NULL, $where)) {
             $result['result'][0]['full_name'] = ucwords($result['result'][0]['firstname'] . " " . $result['result'][0]['midname'] . " " . $result['result'][0]['lastname']);
+            $result['result'][0]['identifier'] = "student";
             print_r(json_encode($result));
         } else if ($result = $this->Crud_model->fetch_array("fic", NULL, $where)) {
+            $result['result'][0]['full_name'] = ucwords($result['result'][0]['firstname'] . " " . $result['result'][0]['midname'] . " " . $result['result'][0]['lastname']);
+            $result['result'][0]['identifier'] = "fic";
             print_r(json_encode($result));
-        } else if ($result = $this->Crud_model->fetch("admin", $where)) {
+        } else if ($result = $this->Crud_model->fetch("prof", $where)) {
+            $result['result'][0]['full_name'] = ucwords($result['result'][0]['firstname'] . " " . $result['result'][0]['midname'] . " " . $result['result'][0]['lastname']);
+            $result['result'][0]['identifier'] = "prof";
             print_r(json_encode($result));
         } else {
             print_r("");
