b594f8c9282c96b531c1a21a09bc3244d461940e
passwordjr <angelojrganaden@gmail.com>
2018-02-14 16:20:22 +0800

    Fixed div on view
    remove col s1 and 11

--- application/views/home-activity.php
+++ application/views/home-activity.php
@@ -4,83 +4,77 @@
 <?php $this->load->view('includes/home-sidenav'); ?>
 
 <div class="row container">
-	<div class="col s1"></div>
-	<div class="col s11">
-		<blockquote class="color-primary-green">
+	<blockquote class="color-primary-green">
 
-			<h1 class="color-black">Activity
-				<a class="btn waves-effect waves-light bg-primary-green modal-trigger" 
-				data-id="<?=$info['user']->id?>" href="#modal_add_activity" id="btn_add_activity">Add Activity</a>
-			</h1>
-		</blockquote>
-	</div>
+		<h3 class="color-black">Activity
+			<a class="btn waves-effect waves-light bg-primary-green modal-trigger" 
+			data-id="<?=$info['user']->id?>" href="#modal_add_activity" id="btn_add_activity">Add Activity</a>
+		</h3>
+	</blockquote>
 </div>
 
 <div class="row container">
 
-	<div class="col s1"></div>
-	<div class="col s11">
-		<?php if ($activity): ?>
-			<?php foreach ($activity as $key => $value): ?>
-				<?php 
+	<?php if ($activity): ?>
+		<?php foreach ($activity as $key => $value): ?>
+			<?php 
 
 		// Activity details
-				$details = $this->Crud_model->fetch("activity_details", array("activity_details_id"=>$value->activity_details_id));
-				$details = $details[0];
-				$subject = $this->Crud_model->fetch("subject", array("subject_id"=>$value->subject_id));
-				$subject = $subject[0];
-				$section = $this->Crud_model->fetch("offering", array("offering_id"=>$subject->offering_id));
-				$section = $section[0];
+			$details = $this->Crud_model->fetch("activity_details", array("activity_details_id"=>$value->activity_details_id));
+			$details = $details[0];
+			$subject = $this->Crud_model->fetch("subject", array("subject_id"=>$value->subject_id));
+			$subject = $subject[0];
+			$section = $this->Crud_model->fetch("offering", array("offering_id"=>$subject->offering_id));
+			$section = $section[0];
 
+			$is_overdue = "";
+			$date = $value->activity_date_time;
+			$now = time();
+
+			if($date < $now) {
+				$is_overdue = "Overdue";
+			}else{
 				$is_overdue = "";
-				$date = $value->activity_date_time;
-				$now = time();
-
-				if($date < $now) {
-					$is_overdue = "Overdue";
-				}else{
-					$is_overdue = "";
-				}
-				?>
-				<div class="col s6">
-					<div class="card bg-primary-green ">
-						<div class="card-content white-text">
-							<div class="row" style="margin-bottom: 0 !important;">
-								<div class="col s8">
-									<blockquote class="color-primary-yellow">
-										<span class="card-title color-white"><?=$details->activity_details_name?> 
-											- <span class="color-red bg-primary-yellow"><?=$is_overdue?></span>
-										</span>
-										<b><?=$section->offering_name." | "?></b><i><?=$subject->subject_name?></i>
-										<p> <?=date("M d, Y | h:i A", $value->activity_date_time)?></p>
-										<p><?=strtoupper($value->activity_venue)?></p>
-									</blockquote>
-									<div class="row div-edit<?=$value->activity_id?>" style="display: none">
-										<input type="text"  class="timepicker time_data time<?=$value->activity_id?>" value="Change Time">
-										<input type="text" class="datepicker date_data date<?=$value->activity_id?>" value="Change Date">
-										<textarea id="txtarea" class="materialize-textarea desc_data"><?=$value->activity_description?></textarea>
-										<label for="txtarea">Description</label>
-										<button class="btn bg-primary-green waves-effect right btn_update_activity" data-id="<?=$value->activity_id?>">Update</button>
-									</div>
-								</div>
-								<div class="col s4">
-									<i class="material-icons right color-white tooltipped btn_delete_activity" data-tooltip="Delete Activity" data-id="<?=$value->activity_id?>" style="cursor: pointer;">delete</i>
-
-									<i class="tooltipped btn_edit_activity material-icons right color-primary-yellow" data-id="<?=$value->activity_id?>" href="#modal_activity" style="cursor: pointer;" data-tooltip="Edit Activity">edit</i>
+			}
+			?>
+			<div class="col s6">
+				<div class="card bg-primary-green ">
+					<div class="card-content white-text">
+						<div class="row" style="margin-bottom: 0 !important;">
+							<div class="col s8">
+								<blockquote class="color-primary-yellow">
+									<span class="card-title color-white"><?=$details->activity_details_name?> 
+										- <span class="color-red bg-primary-yellow"><?=$is_overdue?></span>
+									</span>
+									<b><?=$section->offering_name." | "?></b><i><?=$subject->subject_name?></i>
+									<p> <?=date("M d, Y | h:i A", $value->activity_date_time)?></p>
+									<p><?=strtoupper($value->activity_venue)?></p>
+								</blockquote>
+								<div class="row div-edit<?=$value->activity_id?>" style="display: none">
+									<input type="text"  class="timepicker time_data time<?=$value->activity_id?>" value="Change Time">
+									<input type="text" class="datepicker date_data date<?=$value->activity_id?>" value="Change Date">
+									<textarea id="txtarea" class="materialize-textarea desc_data"><?=$value->activity_description?></textarea>
+									<label for="txtarea">Description</label>
+									<button class="btn bg-primary-green waves-effect right btn_update_activity" data-id="<?=$value->activity_id?>">Update</button>
 								</div>
 							</div>
-							<h5 style="display: block;" class="activity_paragraph_desc<?=$value->activity_id?> a-roboto-cond"><?=$value->activity_description?></h5>
+							<div class="col s4">
+								<i class="material-icons right color-white tooltipped btn_delete_activity" data-tooltip="Delete Activity" data-id="<?=$value->activity_id?>" style="cursor: pointer;">delete</i>
+
+								<i class="tooltipped btn_edit_activity material-icons right color-primary-yellow" data-id="<?=$value->activity_id?>" href="#modal_activity" style="cursor: pointer;" data-tooltip="Edit Activity">edit</i>
+							</div>
 						</div>
-
+						<h5 style="display: block;" class="activity_paragraph_desc<?=$value->activity_id?> a-roboto-cond"><?=$value->activity_description?></h5>
 					</div>
-				</div>
-				<!-- SCRIPT FOR SETTING THE TIME -->
 
-			<?php endforeach ?>
-		<?php else: ?>
-			<h3 class="center">No Data Yet</h3>
-		<?php endif ?>
-	</div>
+				</div>
+			</div>
+			<!-- SCRIPT FOR SETTING THE TIME -->
+
+		<?php endforeach ?>
+	<?php else: ?>
+		<h3 class="center">No Data Yet</h3>
+	<?php endif ?>
 </div>
 
 <div id="modal_add_activity" class="modal" style="height: 100vh">
--- application/views/home.php
+++ application/views/home.php
@@ -13,55 +13,55 @@
 <?php echo "<pre>" ?>
   <?php print_r($info) ?> -->
   <div class="row">
-    <div class="row container">
-      <div class="col s1"></div>
-      <div class="col s11">
+    <div class="container">
          <!--==========================================
         =            Announcement Section            =
         ===========================================-->
 
         <div class="col s4">
-          <h4>Announcements</h4>
-          <div class="row s12">
+         <blockquote class="color-primary-green">
 
+          <h5 class="color-black">Announcement </h5>
+        </blockquote>
+
+        <?php 
+
+        $where = array(
+          "announcement_is_active"=>1
+        );
+        $announcements = $this->Crud_model->fetch("announcement",$where);
+        ?>
+        <?php if ($announcements): ?>
+          <?php foreach ($announcements as $key => $value): ?>
             <?php 
-            
-            $where = array(
-              "announcement_is_active"=>1
-            );
-            $announcements = $this->Crud_model->fetch("announcement",$where);
+            $audience = explode(",",$value->announcement_audience);
+
             ?>
-            <?php if ($announcements): ?>
-              <?php foreach ($announcements as $key => $value): ?>
-                <?php 
-                $audience = explode(",",$value->announcement_audience);
-
-                ?>
-                <?php if (in_array($program, $audience)): ?>
-                  <div class="card bg-primary-green">
-                    <div class="card-content white-text">
-                      <span class="card-title" style="text-transform: capitalize;"><?=$value->announcement_title?></span>
-                      <p style="font-size: 80%" class="color-primary-yellow">Shown Until <?=date("M d, Y", $value->announcement_end_datetime)?></p>
-                      <hr>
-                      <p><?=$value->announcement_content?></p>
-                    </div>
-                  </div>
-                <?php endif ?>
-
-              <?php endforeach ?>
-            <?php else: ?>
+            <?php if (in_array($program, $audience)): ?>
               <div class="card bg-primary-green">
-                <div class="card-content">
-                  <h5 class="center color-white">No Announcement Yet</h5>
+                <div class="card-content white-text">
+                  <span class="card-title" style="text-transform: capitalize;"><?=$value->announcement_title?></span>
+                  <p style="font-size: 80%" class="color-primary-yellow">Shown Until <?=date("M d, Y", $value->announcement_end_datetime)?></p>
+                  <hr>
+                  <p><?=$value->announcement_content?></p>
                 </div>
               </div>
             <?php endif ?>
 
+          <?php endforeach ?>
+        <?php else: ?>
+          <div class="card bg-primary-green">
+            <div class="card-content">
+              <h5 class="center color-white">No Announcement Yet</h5>
+            </div>
           </div>
+        <?php endif ?>
 
-        </div>
 
-        <!--====  End of Announcement Section  ====-->
       </div>
+
+      <!--====  End of Announcement Section  ====-->
+      <div class="col s4 red">sdada</div>
+      <div class="col s4 green"></div>
     </div>
   </div>
