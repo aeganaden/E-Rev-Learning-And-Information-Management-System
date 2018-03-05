9169f6d54bdad71de03f3b009ea52767a3a3bc15
Angelo Ganaden <angelojrganaden@gmail.com>
2018-03-05 17:54:06 +0800

    Added Topics UI

--- application/views/student_scores_import.php
+++ application/views/student_scores_import.php
@@ -116,7 +116,7 @@
                         <div class="col s2"></div>
                     </div>
                 </li>
-                <li class="collection-item white" id="li_s3" style="padding: 5%; margin-bottom: 1%;  display: block;">
+                <li class="collection-item white" id="li_s3" style="padding: 5%; margin-bottom: 1%;  display: none;">
                     <div class="row  ">
                         <h5 class="valign-wrapper">Step
                             <i class="material-icons" style="padding-left: 1%;">filter_3</i>
@@ -130,8 +130,15 @@
 
                         <div class="col s2"></div>
                         <div class="col s8">
+                            <blockquote class="color-primary-green">
+                                <h6 class="color-black"><b>Selected Topics :</b></h6>
+                            </blockquote>
                             <div class="row">
+                                <div class="col s4"></div>
+                                <div class="col s4"  id="show_list">
 
+                                </div>
+                                <div class="col s4"></div>
                             </div>
                             <div class="row">
                              <div class="col s4"></div>
@@ -144,7 +151,7 @@
                     <div class="col s2"></div>
                 </div>
             </li>
-            <li class="collection-item white" id="li_s3" style="padding: 5%; margin-bottom: 1%;  display: none;">
+            <li class="collection-item white" id="li_s4" style="padding: 5%; margin-bottom: 1%;  display: none;">
                 <div class="row  ">
                     <h5 class="valign-wrapper">Step
                         <i class="material-icons" style="padding-left: 1%;">filter_4</i>
@@ -177,27 +184,38 @@
         $active_enrollment = $this->Crud_model->fetch("enrollment",array("enrollment_is_active"=>1));
         $active_enrollment = $active_enrollment[0];
         // fetch course
-        $course = $this->Crud_model->fetch("course",array("enrollment_id"=>$active_enrollment->enrollment_id));
+        $course = $this->Crud_model->fetch("course",array("enrollment_id"=>$active_enrollment->enrollment_id,"course_department"=>$info['user']->fic_department));
         // fetch subjects 
         ?>
-        <?php if($course): ?>
-            <?php foreach ($course as $key => $value): ?>
-                <?php 
-                $subjects = $this->Crud_model->fetch("subject",array("course_id"=>$value->course_id));
+        <div class="row">
+            <?php if($course): ?>
+                <?php foreach ($course as $key => $value): ?>
+                    <?php 
+                    $subjects = $this->Crud_model->fetch("subject",array("course_id"=>$value->course_id));
                 // fetch topic
-                ?>
-                <?php if ($subjects): ?>
-                 <?php foreach ($subjects as $key => $i_value): ?>
-                  <?php 
-                  $topic = $this->Crud_model->fetch("topic",array("subject_id"=>$i_value->subject_id));
-                  ?>
-              <?php endforeach ?>
-          <?php endif ?>
-      <?php endforeach ?>
-  <?php endif ?>
+                    ?>
+                    <?php if ($subjects): ?>
+                     <?php foreach ($subjects as $key => $i_value): ?>
+                      <?php 
+                      $topic = $this->Crud_model->fetch("topic",array("subject_id"=>$i_value->subject_id));
+                      ?>
+                      <?php if ($topic): ?>
+                          <?php foreach ($topic as $key => $j_value): ?>
+                              <div class="col s3">
+                                <p><input type="checkbox" class="filled-in topic_list_c" name="topic_list[]" id="<?=$j_value->topic_id?>" />
+                                    <label id="lbl_t_<?=$j_value->topic_id?>" for="<?=$j_value->topic_id?>"><?=$j_value->topic_name?></label>
+                                </p>
+                            </div>
+                        <?php endforeach ?>
+                    <?php endif ?>
+                <?php endforeach ?>
+            <?php endif ?>
+        <?php endforeach ?>
+    <?php endif ?>
+</div>
 </div>
 <div class="modal-footer">
-    <a href="#!" type="button" class="modal-action modal-close waves-effect waves-light btn bg-primary-green ">Agree</a>
+    <a href="#!" type="button" id="btn_add_topic" class="modal-action modal-close waves-effect waves-light btn bg-primary-green ">Agree</a>
 </div>
 </div>
 
@@ -245,6 +263,30 @@
             }
         });
 
+
+        // Add topic to list
+        $("#btn_add_topic").click(function(event) {
+            var data_html = "";
+            $('.topic_list_c:checkbox:checked').each(function () {
+                var sThisVal = (this.checked ? $(this).attr("id") : "");
+                var label = $("#lbl_t_"+sThisVal).html();
+                data_html += "*"+label + "<br>" ;
+            });
+            $("#show_list").html(data_html);
+
+            if ($("#show_list").html()=="") {
+                $toast = '<span>Must Choose Topic</span>';
+                Materialize.toast($toast, 2000);
+
+                $('#li_s4').fadeOut('fast', function () {
+                    $("#li_s4").css('display', 'none');
+                });
+            }else{
+             $('#li_s4').fadeIn('fast', function () {
+                $("#li_s4").css('display', 'block');
+            });
+         }
+     });
 
         $("#btn_submit_ss").click(function (event) {
 
