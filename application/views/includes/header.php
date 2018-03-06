780a680ff39cdda6249d79e1be91528cec169a5a
passwordjr <angelojrganaden@gmail.com>
2018-03-06 17:13:01 +0800

    Added favicon

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title><?= $title ?></title>
+	<link rel="shortcut icon" type="image/png" href="<?=base_url()?>assets/img/favicon.png"/>
 
 	<!--Let browser know website is optimized for mobile-->
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
