8f87419e8d3442004108f6e69db9a9149ef6172b
passwordjr <angelojrganaden@gmail.com>
2018-03-20 12:13:52 +0800

    Updated ui fic course modules

--- application/controllers/ManageCourseModules.php
+++ application/controllers/ManageCourseModules.php
@@ -211,6 +211,60 @@ class ManageCourseModules extends CI_Controller {
 			force_download($file, NULL);
 		}
 	}
+
+	public function viewModules()
+	{
+		$info = $this->session->userdata('userInfo');
+
+		$ident = $info['identifier'];
+		$ident.="_department";
+		$program = 0;
+
+		switch ($info['user']->$ident) {
+			case 'CE':
+			$program = 1;
+			break;
+			case 'ECE':
+			$program = 2;
+			break;
+			case 'EE':
+			$program = 3;
+			break;
+			case 'ME':
+			$program = 4;
+			break;
+
+			default:
+			break;
+		}
+
+
+
+		if ($info['logged_in'] && ($info['identifier'] == "fic")) {
+			$data = array(
+				"title" => "Course Modules - Learning Management System | FEU - Institute of Techonology",
+				"info" => $info,
+				"program" => $program,
+				"s_h" => "",
+				"s_a" => "",
+				"s_f" => "",
+				"s_c" => "",
+				"s_t" => "",
+				"s_s" => "",
+				"s_co" => "",
+				"s_ss" => "",
+				"s_ga" => "",
+				"s_rc" => "selected-nav",
+			);
+			$this->load->view('includes/header', $data);
+			$this->load->view('course_modules/mv_course_modules');
+			$this->load->view('includes/footer');
+		} elseif ($info['identifier'] == "administrator") {
+			redirect('Admin');
+		} else {
+			redirect('Welcome', 'refresh');
+		}
+	}
 }
 /* End of file ManageCourseModules.php */
 /* Location: ./application/controllers/ManageCourseModules.php */
