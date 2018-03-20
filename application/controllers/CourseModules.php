46530624b49190a02358ecf5c7385a83cf59baa6
passwordjr <angelojrganaden@gmail.com>
2018-03-20 11:54:14 +0800

    Updated course module ui student

--- application/controllers/CourseModules.php
+++ application/controllers/CourseModules.php
@@ -123,6 +123,60 @@ class CourseModules extends CI_Controller {
 		}
 	}
 
+	public function viewModules()
+	{
+		$info = $this->session->userdata('userInfo');
+
+		$ident = $info['identifier'];
+		$ident.="_department";
+		$program = 0;
+
+		switch ($info['user']->$ident) {
+			case 'CE':
+			$program = 1;
+			break;
+			case 'ECE':
+			$program = 2;
+			break;
+			case 'EE':
+			$program = 3;
+			break;
+			case 'ME':
+			$program = 4;
+			break;
+
+			default:
+			break;
+		}
+
+
+
+		if ($info['logged_in'] && ($info['identifier'] == "student")) {
+			$data = array(
+				"title" => "Course Modules - Learning Management System | FEU - Institute of Techonology",
+				"info" => $info,
+				"program" => $program,
+				"s_h" => "",
+				"s_a" => "",
+				"s_f" => "",
+				"s_c" => "",
+				"s_t" => "",
+				"s_s" => "",
+				"s_co" => "",
+				"s_ss" => "",
+				"s_ga" => "",
+				"s_rc" => "selected-nav",
+			);
+			$this->load->view('includes/header', $data);
+			$this->load->view('course_modules/v_course_modules');
+			$this->load->view('includes/footer');
+		} elseif ($info['identifier'] == "administrator") {
+			redirect('Admin');
+		} else {
+			redirect('Welcome', 'refresh');
+		}
+	}
+
 }
 
 /* End of file CourseModules.php */
--- application/views/course_modules/course_modules.php
+++ application/views/course_modules/course_modules.php
@@ -13,33 +13,29 @@
 		$course = $this->Crud_model->fetch("course",
 			array("course_department"=>$info['user']->student_department,
 				"enrollment_id"=>$enrollment->enrollment_id));
-				?> 
-				<?php if ($course): ?>
-					<?php foreach ($course as $key => $value): ?>
-						<!-- fetch subject -->
-						<?php $subject = $this->Crud_model->fetch("subject",array("course_id"=>$value->course_id)); ?>
-						<?php if ($subject): ?>
-							<?php $counter = 0; ?>
-							<?php foreach ($subject as $key => $i_value): ?>
-								<?php $counter == 2 ? "<div class='row'>" : "" ?>
-									<div class="col s12 m6">
-										<div class="card bg-primary-green">
-											<div class="card-content white-text">
-												<span class="card-title "><?=$i_value->subject_name?></span>
-												<p><?=$i_value->subject_description?></p>
-											</div>
-											<div class="card-action">
-												<a href="<?=base_url()?>CourseModules/viewCourseModules/<?=$i_value->subject_id?>" class="valign-wrapper"> <i class="material-icons left">visibility</i>View</a>
-											</div>
-										</div>
+		$count = 0;
+		?> 
+		<?php if ($course): ?>
+			<?php foreach ($course as $key => $value): ?>
+				<?php if ($count == 3): ?><?= "<div class='row'>" ?><?php endif ?>
+					<div class="col s4" >
+						<div class="row">
+							<div class="col s12">
+								<div class="card bg-primary-green">
+									<div class="card-content white-text">
+										<span class="card-title"><?= $value->course_course_code ?></span>
+										<p><?= $value->course_course_title ?>.</p>
 									</div>
-									<?php $counter == 2 ? "</div>" : "" ?>
-									<?php $counter = $counter + 1; ?>
-								<?php endforeach ?>
-
-							<?php endif ?>
-						<?php endforeach ?>
-					<?php endif ?>
-				</div>
-			</div>
-			
+									<div class="card-action ">
+										<a href="<?= base_url() ?>CourseModules/viewModules/<?= $value->course_id ?>" class="valign-wrapper">View<i class="material-icons" style="padding-left: 5%">remove_red_eye</i></a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<?php if ($count == 3): ?><?= "</div>" ?><?php endif ?>
+					<?php $count++; ?>
+				<?php endforeach ?>
+			<?php endif ?>
+		</div>
+	</div>
