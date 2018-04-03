59e9108495ac82646efc8091fbede22a1a6251ca
passwordjr <angelojrganaden@gmail.com>
2018-04-03 15:44:05 +0800

    Updated config xss

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   |
  */
 
-  $config['base_url'] = 'http://localhost/Engineering/';
+  $config['base_url'] = 'http://feu-erev.esy.es/';
 
 
 /*
