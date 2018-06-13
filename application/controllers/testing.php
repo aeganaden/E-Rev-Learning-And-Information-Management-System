2d814ab1000c0c0eef261a63599a3dd52aa063d8
markdenverPH <babaranmark@yahoo.com>
2018-06-13 23:56:15 +0800

    FCM push notif
    - fully func
    - apply to announcement module

--- application/controllers/testing.php
+++ application/controllers/testing.php
@@ -26,6 +26,7 @@ class testing extends CI_Controller {
         //     array_push($tokens, $value->token);
         // }
         $tokens[] = "f8xgxGA61hE:APA91bEF0cHlHwPIZCV0GWTCRKUUqTpMAeaHomRS_iF3xb0bs_vgOdajvsAer9sVsaFLDauNtWOzfD3Ijn3dgbOuzQA_wEB3AOOxpeGpjXM6iu0s5_rQLbcGzki1yOFoz7l06u0KVNFW";
+        $tokens[] = "fVi6djEk3XA:APA91bGdo98MvNguKPWmcpajzXchIm8F08JipP2W6Rl8v0LHw8tu_g5UGRE6wy1qkFGaYSGTGfIy0JYQEjnF7ravATQR_myddQJSPheA2rxiGbDX95xkkTg_aQxrglEQa4o2MuZyWuFF";
         $data = array(
             'operation' => 'create',
             'notification_key_name' => random_string('alnum'),
@@ -47,56 +48,54 @@ class testing extends CI_Controller {
         $result = json_decode($result);
 
         //send notification
-        $data = array(
-            'notification' => array('body' => 'Check it out', 
-                'title' => 'New Announcement from Management',
-                'sound' => 'default',
-                'color' => 'black',
-                'priority' => 'max'),
-            'to'    => $result->notification_key
-        );
+        // $data = array(
+        //     'notification' => array('body' => 'Check it out', 
+        //         'title' => 'New Announcement from Management',
+        //         'sound' => 'default',
+        //         'color' => 'black',
+        //         'priority' => 'max'),
+        //     'to'    => $result->notification_key
+        // );
 
-        $data_string = json_encode($data);
+        // $data_string = json_encode($data);
 
-        $curl = curl_init('https://fcm.googleapis.com/fcm/send');
+        // $curl = curl_init('https://fcm.googleapis.com/fcm/send');
 
-        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
+        // curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
 
-        curl_setopt($curl, CURLOPT_HTTPHEADER,
-            array(
-                'Content-Type: application/json',
-                'Authorization: key=AIz') // api key ng firebase
-        );
+        // curl_setopt($curl, CURLOPT_HTTPHEADER,
+        //     array(
+        //         'Content-Type: application/json',
+        //         'Authorization: key=AIz') // api key ng firebase
+        // );
 
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
-        $result = curl_exec($curl);
-        curl_close($curl);
-        echo $result;
+        // curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        // curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+        // $result = curl_exec($curl);
+        // curl_close($curl);
+        // echo $result;
     }
 
     public function testing2(){
-        #API access key from Google API's Console
-
-        $registrationIds[] = "f8xgxGA61hE:APA91bEF0cHlHwPIZCV0GWTCRKUUqTpMAeaHomRS_iF3xb0bs_vgOdajvsAer9sVsaFLDauNtWOzfD3Ijn3dgbOuzQA_wEB3AOOxpeGpjXM6iu0s5_rQLbcGzki1yOFoz7l06u0KVNFW";
-        // $registrationIds = json_encode($registrationIds);
-        #prep the bundle
+        $registrationIds[] = "fqRFW3I6EDc:APA91bF8mDoIhgK0_HaPVSltL4IzEznubgWU988gaXJ6-OzluccnevgkqkN50LwQeCLbY9NmbDymHe2-QGTr2hANbwzZvuXNCykOnjs4UQj7wYmD8yiW0PiQ_t-xJvnY9SP40KdCr8bB";
+        $registrationIds[] = "fq36ApRShSM:APA91bFJsYkiMHvlmkwssJTVPKgani9edxghOVcRJe_6rNdA3Z_dV1mCTUhr1yLJ6z_eM9Q-k9T7KS9nw91Mmzt0cMddag64PAOyYC4grcdkIK925CGzjz9EAEfix0nyee6mFVDn0Y4D";
+        // prep the bundle
         $msg = array(
-            'body'  => 'Body  Of Notification',
-            'title' => 'Title Of Notification',
-            'icon'  => 'myicon',/*Default Icon*/
-            'sound' => 'mySound'/*Default sound*/
+            'body'   => 'This is a sample announcement77',
+            'title'     => 'Announcement from QA77'
         );
         $fields = array(
-            'to'        => $registrationIds,
-            'notification'  => $msg
+            'registration_ids' => $registrationIds,
+            'notification' => $msg,
+            'data' => array('test'=>"this is test")
         );
 
         $headers = array(
-            'Authorization: key=' . API_ACCESS_KEY,
-            'Content-Type: application/json'
+            'Authorization: key=AIzaSyDHyR3pW8tUInWPa-SD1odWq7WRyiI6z5k',
+            'Content-Type: application/json',
+            'project_id: 756991022347'
         );
-        #Send Reponse To FireBase Server    
+
         $ch = curl_init();
         curl_setopt( $ch,CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send' );
         curl_setopt( $ch,CURLOPT_POST, true );
@@ -106,7 +105,6 @@ class testing extends CI_Controller {
         curl_setopt( $ch,CURLOPT_POSTFIELDS, json_encode( $fields ) );
         $result = curl_exec($ch );
         curl_close( $ch );
-        #Echo Result Of FireBase Server
         echo $result;
     }
 }
