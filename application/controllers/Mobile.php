068b921ac93d124d473be1b107b9ac8bd1c77496
markdenverPH <babaranmark@yahoo.com>
2018-06-20 23:35:24 +0800

    attendance module
    - on process

--- application/controllers/Mobile.php
+++ application/controllers/Mobile.php
@@ -71,7 +71,7 @@ class Mobile extends CI_Controller {
             print_r("");
         }
     }
-    
+
 }
 
 public function announcement() {
@@ -761,6 +761,170 @@ public function feedback_fetch() {
         $result = $this->Crud_model->fetch_join2("lecturer as lec", $col, $join, NULL, $where, true);
         $hold["result"] = $result;
         print_r(json_encode($hold));
+    }
+
+    public function fetch_lecturer_attendance(){
+        // $_POST['department'] = "CE";
+        // $_POST['id'] = 1;
+
+        $lec_id = $_POST['id'];
+        $dept = $_POST['department'];
+
+        $col = "cou.course_id";
+        $where = array(
+            "enr.enrollment_is_active" => 1,
+            "cou.course_department" => $dept
+        );
+        $join = array(
+            array("enrollment as enr", "enr.enrollment_id = cou.enrollment_id")
+        );
+        $course = $this->Crud_model->fetch_join2("course as cou", $col, $join, NULL, $where);
+
+        // necessary initialization
+        $sum = 0;
+        $total_hours = 0;
+        $interval = "";
+        $test = array();
+        $total_time = array();
+        $x = 0;
+        // create new object
+        $attendance_data = array();
+
+        // create array of object
+        $attendance = array();
+
+
+        if (!empty($course)) {
+            foreach ($course as $key => $value) {
+                // fetch offering with course id that is active
+                $offering = $this->Crud_model->fetch("offering", array("course_id" => $value->course_id));
+                if ($offering) {
+                    foreach ($offering as $key => $inner_value) {
+
+                        // fetch lecturer
+                        $lec_attendance = $this->Crud_model->fetch("lecturer_attendance", array("lecturer_id" => $lec_id, "offering_id" => $inner_value->offering_id));
+                        if ($lec_attendance) {
+                            foreach ($lec_attendance as $key => $lec) {
+                                // echo $x++;
+                                // fetch here then enclose the insertion data to foreach of schedule
+                                // fetch schedule
+                                $schedule = $this->Crud_model->fetch("schedule", array("schedule_id" => $lec->schedule_id));
+                                $schedule = $schedule[0];
+
+                                // Schedule start and time_in difference
+                                $s_t_start = date_create(date("h:i a", $schedule->schedule_start_time));
+                                $s_t_end = date_create(date("h:i a", $schedule->schedule_end_time));
+
+                                $l_a_s = $this->Crud_model->fetch("attendance_in", array("lecturer_attendance_id" => $lec->lecturer_attendance_id));
+                                foreach ($l_a_s as $key => $value) {
+                                    $l_a_in = date_create(date("h:i a", $value->attendance_in_time));
+                                    $d_in = date_diff($s_t_start, $l_a_in);
+                                    $l_a_e = $this->db->select('*')->where(array("lecturer_attendance_id" => $value->lecturer_attendance_id))->order_by('attendance_out_id', "desc")->limit(1)->get('attendance_out')->row();
+                                    $l_a_out = date_create(date("h:i a", $l_a_e->attendance_out_time));
+                                    $d_out = date_diff($s_t_end, $l_a_out);
+
+                                    // Remarks Section
+                                    $padding_start = 20;
+                                    $padding_start_late = 20;
+                                    $padding_end_late = 20;
+                                    $remarks_s = "";
+                                    $remarks_e = "";
+                                    // remove all % to compare
+                                    $lain = str_replace("%", "", $l_a_in->format("%h%i"));
+                                    $sain = str_replace("%", "", $s_t_start->format("%h%i"));
+                                    if ($sain > $lain) {
+                                        $d_in_f = $d_in->format('%h%i') * -1;
+                                    } else {
+                                        $d_in_f = $d_in->format('%h%i') * 1;
+                                    }
+
+
+                                    if ($d_in_f == 0) {
+                                        $remarks_s = "Exact Time In";
+                                    } elseif ($d_in_f < 0 && $d_in_f > ($padding_start * -1)) {
+                                        $remarks_s = "Early Time In";
+                                    } elseif ($d_in_f > 0 && $d_in_f < $padding_start_late) {
+                                        $remarks_s = "Late Time In";
+                                    } else {
+                                        $remarks_s = "-";
+                                    }
+
+                                    $laout = str_replace("%", "", $l_a_out->format("%h%i"));
+                                    $saout = str_replace("%", "", $s_t_end->format("%h%i"));
+                                    if ($saout > $laout) {
+                                        $d_out_f = $d_out->format('%h%i') * -1;
+                                    } else {
+                                        $d_out_f = $d_out->format('%h%i') * 1;
+                                    }
+                                    if ($d_out_f == 0) {
+                                        $remarks_e = "Exact Time Out";
+                                    } elseif ($d_out_f < 0) {
+                                        $remarks_e = "Early Dismissal";
+                                    } elseif ($d_out_f > 0 && $d_out_f < $padding_end_late) {
+                                        $remarks_e = "Late Dismissal";
+                                    } else {
+                                        $remarks_e = "Overtime";
+                                    }
+
+                                    if ($remarks_s == "-") {
+                                        $remarks_e = "";
+                                    }
+                                    $attendance_data['lecturer_attendance_id'] = $lec->lecturer_attendance_id;
+                                    $attendance_data['lecturer_attendance_date'] = $lec->lecturer_attendance_date;
+                                    $attendance_data['lecturer_attendance_in'] = $value->attendance_in_time;
+                                    $attendance_data['lecturer_attendance_out'] = $l_a_e->attendance_out_time;
+                                    $attendance_data['sched_start'] = $schedule->schedule_start_time;
+                                    $attendance_data['sched_end'] = $schedule->schedule_end_time;
+                                    $attendance_data['remarks_s'] = $remarks_s;
+                                    $attendance_data['remarks_e'] = $remarks_e;
+                                    // echo "<pre>";
+                                    // print_r($attendance);
+
+                                    $lec_in = date("h:i", $value->attendance_in_time);
+                                    $lec_out = date("h:i", $l_a_e->attendance_out_time);
+                                    $stend = date("h:i", $schedule->schedule_end_time);
+                                    $interval = $this->diff($lec_in, $lec_out);
+                                    if ($remarks_e == "Overtime" && $remarks_s != "-") {
+                                        $interval = $this->diff($lec_in, $stend);
+                                        // echo $lec_in."---".$stend."<br>";
+                                    }
+                                    $sum = $interval['h'] . ":" . $interval['i'];
+                                    $attendance_data['hours_rendered'] = $sum;
+
+                                    if ($remarks_s == "-") {
+                                        $attendance_data['hours_rendered'] = 0;
+                                        $sum = "0:0";
+                                    }
+                                    $attendance[] = $attendance_data;
+                                    array_push($total_time, $sum);
+                                }
+                                // echo $d_out->format('%r%i');
+                                // echo "<br>";
+                            }
+                        }
+                    }
+                }
+            }
+            $final_data["result"] = $attendance;
+            echo "<pre>";
+            print_r(json_encode($final_data));
+        } else {
+            print_r(json_encode(""));
+        }
+    }
+
+    private function diff($date1, $date2, $format = false) {
+        $diff = date_diff(date_create($date1), date_create($date2));
+        if ($format)
+            return $diff->format($format);
+
+        return array('y' => $diff->y,
+            'm' => $diff->m,
+            'd' => $diff->d,
+            'h' => $diff->h,
+            'i' => $diff->i,
+            's' => $diff->s
+        );
     }
 
     private function get_active_enrollment() {
