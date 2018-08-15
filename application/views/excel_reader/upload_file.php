66dc08acdab5649fd43338cf10b387f48ee21766
markdenverPH <markdenverbabaran@gmail.com>
2018-08-16 00:16:01 +0800

    batch upload for excel

--- application/views/excel_reader/upload_file.php
+++ application/views/excel_reader/upload_file.php
@@ -3,7 +3,7 @@
     <br><br>
     <?php
 if (!empty($error)) {
-	echo "<span class=\"red-text\" >" . $error . "</span>";
+    echo "<span class=\"red-text\" >" . $error . "</span>";
 }
 ?>
    <br><br>
@@ -50,47 +50,37 @@ if (!empty($error)) {
     </center>
     <form id="attendanceForm" method="post" enctype="multipart/form-data">
         <div class="row">
-            <div class="input-field col s6">
-                <select id="sel_sched" name="sched">
-                    <option value="" disabled selected>Select the Schedule</option>
-                    <?php
-foreach ($lects as $lect) {
-	echo "<option value='$lect->sched_id'>$lect->fullname — $lect->lec_id " . date('(D g:iA-', $lect->start_time) . date('g:iA)', $lect->end_time) . "</option>";
-}
-?>
-               </select>
-           </div>
-           <div class="file-field input-field col s6">
-            <div class="btn">
-                <span>File</span>
-                <input type="file" name="userfile"/> <!-- put required -->
-            </div>
-            <div class="file-path-wrapper">
-                <input class="file-path validate" type="text">
-            </div>
-
-            <!-- Modal Trigger -->
-            <div class="input-field">
-                <a class="waves-effect waves-light btn modal-trigger right" href="#modal2">Next</a>
-            </div>
-
-            <!-- Modal Structure -->
-            <div id="modal2" class="modal">
-                <div class="modal-content">
-                    <h4><span class='red-text'>WARNING</span></h4>
-                    <p>Are you sure you want to upload this </p>
+            <div class="file-field input-field">
+                <div class="btn">
+                    <span>File</span>
+                    <input type="file" name="userfile"/> <!-- put required -->
                 </div>
-                <div class="modal-footer">
-                    <button type="submit" value="upload" class="modal-action modal-close waves-effect waves-light btn right">
-                        proceed
-                    </button>
-                    <button type="button" class="modal-action modal-close waves-effect red waves-light btn">
-                        cancel
-                    </button>
+                <div class="file-path-wrapper">
+                    <input class="file-path validate" type="text">
+                </div>
+
+                <!-- Modal Trigger -->
+                <div class="input-field">
+                    <a class="waves-effect waves-light btn modal-trigger right" href="#modal2">Next</a>
+                </div>
+
+                <!-- Modal Structure -->
+                <div id="modal2" class="modal">
+                    <div class="modal-content">
+                        <h4><span class='red-text'>WARNING</span></h4>
+                        <p>Are you sure you want to upload this </p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" value="upload" class="modal-action modal-close waves-effect waves-light btn right">
+                            proceed
+                        </button>
+                        <button type="button" class="modal-action modal-close waves-effect red waves-light btn">
+                            cancel
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</form>
+    </form>
 </div>
 </div>
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
-$(document).ready(function() {
+$(document).ready(function () {
 	$(".button-collapse").sideNav({
-		onOpen: function(el) { 
-			$("#slide-out").css("display","block");
+		onOpen: function (el) {
+			$("#slide-out").css("display", "block");
 		},
 	});
 	$('.modal').modal({
-		dismissible: true, 
-		complete: function() { 
+		dismissible: true,
+		complete: function () {
 			if ($(this).attr("id") == "modal_cw") {
 				$("#cw_title").val("");
 				$("#cw_des").val("");
@@ -18,81 +18,78 @@ $(document).ready(function() {
 				CKEDITOR.instances['answer_3'].setData(' ');
 				CKEDITOR.instances['answer_4'].setData(' ');
 			}
-       } // Callback for Modal close
-   });
-	
+		} // Callback for Modal close
+	});
+
 	$('.collapsible').collapsible();
 	$('select').material_select();
 	$('ul.tabs').tabs();
 	$(".dropdown-button").dropdown();
 	$('.slider').slider({
-		indicators:false,
+		indicators: false,
 		height: 500
 	});
 	$('input#input_text, textarea#reg_expertise').characterCounter();
-	
+
 	// Date now and tomorrow
 	var today = new Date();
 	var tomorrow = new Date();
-	tomorrow.setDate(today.getDate()+1); 
+	tomorrow.setDate(today.getDate() + 1);
 
 
 	$('.datepicker').pickadate({
-	    selectMonths: true, // Creates a dropdown to control month
-	    selectYears: 15, // Creates a dropdown of 15 years to control year,
-	    today: false,
-	    clear: 'Clear',
-	    close: 'Ok',
-	    hiddenName: true,
-	    min: tomorrow,
-	    format: 'mmmm d, yyyy',
-	    closeOnSelect: true // Close upon selecting a date,
+		selectMonths: true, // Creates a dropdown to control month
+		selectYears: 15, // Creates a dropdown of 15 years to control year,
+		today: false,
+		clear: 'Clear',
+		close: 'Ok',
+		hiddenName: true,
+		min: tomorrow,
+		format: 'mmmm d, yyyy',
+		closeOnSelect: true // Close upon selecting a date,
 	});
 	$('.timepicker').pickatime({
-	    default: 'now', // Set default time: 'now', '1:30AM', '16:30'
-	    fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
-	    twelvehour: true, // Use AM/PM or 24-hour format
-	    donetext: 'OK', // text for done-button
-	    cleartext: 'Clear', // text for clear-button
-	    canceltext: 'Cancel', // Text for cancel-button
-	    autoclose: true, // automatic close timepicker
-	    ampmclickable: true, // make AM PM clickable
-	    aftershow: function(){} //Function for after opening timepicker
+		default: 'now', // Set default time: 'now', '1:30AM', '16:30'
+		fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
+		twelvehour: true, // Use AM/PM or 24-hour format
+		donetext: 'OK', // text for done-button
+		cleartext: 'Clear', // text for clear-button
+		canceltext: 'Cancel', // Text for cancel-button
+		autoclose: true, // automatic close timepicker
+		ampmclickable: true, // make AM PM clickable
+		aftershow: function () { } //Function for after opening timepicker
 	});
 	initiateOnClick();
 	schedule();
 	navigations();
 
 	//mark's
-	$('#attendanceForm').submit(function(e){
+	$('#attendanceForm').submit(function (e) {
 		e.preventDefault();
 		$.ajax({
-			url:BASE_URL+'Importdata/attendanceUpload/',
-			type:"post",
+			url: BASE_URL + 'Importdata/attendanceUpload2/',
+			type: "post",
 			data: new FormData(this),
-			processData:false,
-			contentType:false,
-			cache:false,
+			processData: false,
+			contentType: false,
+			cache: false,
 			dataType: 'json',
-			// async:false,
-			success: function(data){
-				// console.log(data);
-
-				if(data == "true"){
+			success: function (data) {
+				if (data == "true") {
 					swal({
 						title: "Success",
 						text: "Successfully inserted the data.",
 						icon: "success",
 						buttons: "Ok",
-					}).then((success)=>{
+					}).then((success) => {
 						window.location.href = BASE_URL + "Admin";
 					});
-				} else if(data == "not_admin") {
+				} else if (data == "not_admin") {
 					window.location.href = BASE_URL;
-				}  else if(data == "row1") {	//error on row1 of excel
+				} else if (data == "row1") {	//error on row1 of excel
 					swal({
 						title: "Error",
-						text: "Row 1 should be written like this: EmpID | Date | In | Out",
+						text: "Row 1 should be written like this: EmpID | Date | In | Out | Shed In | Sched Out | Sched Day",
 						icon: "error",
 						buttons: "Ok",
 					});
@@ -104,7 +101,7 @@ $(document).ready(function() {
 						buttons: "Ok",
 					});
 				}
-			}, error: function(data){
+			}, error: function (data) {
 				swal({
 					title: "Error",
 					text: "An unexpected error occured while processing the data. Try refreshing this webpage and try again.",
@@ -124,33 +121,33 @@ $(document).ready(function() {
 ===================================*/
 
 function schedule() {
-	
 
-	$("#sections").change(function(){
+
+	$("#sections").change(function () {
 		$.ajax({
-			url: base_url+"Welcome/fetchSchedule",
+			url: base_url + "Welcome/fetchSchedule",
 			type: 'post',
 			dataType: 'json',
-			data: {id: $(this).val()},
-			success: function(data){
-				if (data==false) {
+			data: { id: $(this).val() },
+			success: function (data) {
+				if (data == false) {
 					$("#schedule_section").html("<div class='col s4'></div><div class='col s4'><h4>No Attendance Encoded Yet</h4></div><div class='col s4'></div>");
-				}else{
+				} else {
 					var calendar = new Timetable();
 					calendar.setScope(7, 21);
 					calendar.addLocations(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']);
-					for(var i =0; i < data.length ; i++){
+					for (var i = 0; i < data.length; i++) {
 
-					// console.log(data[i]);	
-					calendar.addEvent(data[i].schedule_venue, 	data[i].day, new Date(2015,7,data[i].s_d,data[i].s_h,data[i].s_min), new Date(2015,7,data[i].e_d,data[i].e_h,data[i].e_min));
-					var renderer = new Timetable.Renderer(calendar);
-					renderer.draw('.timetable'); 
-					
+						// console.log(data[i]);	
+						calendar.addEvent(data[i].schedule_venue, data[i].day, new Date(2015, 7, data[i].s_d, data[i].s_h, data[i].s_min), new Date(2015, 7, data[i].e_d, data[i].e_h, data[i].e_min));
+						var renderer = new Timetable.Renderer(calendar);
+						renderer.draw('.timetable');
+
+					}
 				}
-			}
 
-		}
-	});
+			}
+		});
 
 
 	});
@@ -164,33 +161,33 @@ function schedule() {
 
 function navigations() {
 	//Navigation Button
-	$("#nav_schedule").click(function() {
+	$("#nav_schedule").click(function () {
 		$('html, body').animate({
 			scrollTop: $("#nav_schedule_sec").offset().top
 		}, 500);
 	});
-	$("#nav_features").click(function() {
+	$("#nav_features").click(function () {
 		$('html, body').animate({
 			scrollTop: $("#nav_features_sec").offset().top
 		}, 500);
 	});
-	$("#nav_benefits").click(function() {
+	$("#nav_benefits").click(function () {
 		$('html, body').animate({
 			scrollTop: $("#nav_benefits_sec").offset().top
 		}, 500);
 	});
-	$("#nav_statements").click(function() {
+	$("#nav_statements").click(function () {
 		$('html, body').animate({
 			scrollTop: $("#nav_statements_sec").offset().top
 		}, 500);
 	});
-	$("#nav_login").click(function() {
+	$("#nav_login").click(function () {
 		$('html, body').animate({
 			scrollTop: $("#nav_login_sec").offset().top
 		}, 500);
 	});
 
-	$("#nav_top").click(function() {
+	$("#nav_top").click(function () {
 		$('html, body').animate({
 			scrollTop: $("#top").offset().top
 		}, 500);
@@ -208,7 +205,7 @@ function navigations() {
 ==================================*/
 
 function showonlyone(thechosenone) {
-	$('.div_section').each(function(index) {
+	$('.div_section').each(function (index) {
 		if ($(this).attr("id") == thechosenone) {
 			$(this).show(200);
 		}
@@ -223,12 +220,12 @@ function showonlyone(thechosenone) {
 
 
 
-function getDivHeightClass(x){	
-	var height = $("."+x).height();
+function getDivHeightClass(x) {
+	var height = $("." + x).height();
 	return height;
 }
-function getDivHeightID(x){	
-	var height = $("#"+x).height();
+function getDivHeightID(x) {
+	var height = $("#" + x).height();
 	return height;
 }
 
@@ -236,10 +233,10 @@ function getDivHeightID(x){
 =            Datatables JS            =
 =====================================*/
 
-$(document).ready(function() {
+$(document).ready(function () {
 	$('.data-table').DataTable();
-	
-} );
+
+});
 
 /*=====  End of Datatables JS  ======*/
 
@@ -249,7 +246,7 @@ $(document).ready(function() {
 =            Login Modal JS            =
 ======================================*/
 
-$("#login-password").keydown(function(event) {
+$("#login-password").keydown(function (event) {
 	if (event.keyCode == 13) {
 		login_verify();
 	}
@@ -267,90 +264,90 @@ function login_verify() {
 	$username = $("#login-username").val();
 	$password = $("#login-password").val();
 	$.ajax({
-		url: base_url+"Login/verify",
+		url: base_url + "Login/verify",
 		type: "post",
 		dataType: "json",
-		data:{
+		data: {
 			username: $username,
 			password: $password
 		},
-		success: function(data){
-			if(data == "68d5fef94c7754840730274cf4959183b4e4ec35"){
+		success: function (data) {
+			if (data == "68d5fef94c7754840730274cf4959183b4e4ec35") {
 				$.ajax({
-						// professor
-						url: base_url+"Login/redirect/68d5fef94c7754840730274cf4959183b4e4ec35",
-						type: "post",
-						dataType: "json",
-						data:{
-							username: $username,
-						},
-						success: function(data){
-							window.location.href = data;	
-						},
-						error: function(data){
-							console.log(data);	
-						}
+					// professor
+					url: base_url + "Login/redirect/68d5fef94c7754840730274cf4959183b4e4ec35",
+					type: "post",
+					dataType: "json",
+					data: {
+						username: $username,
+					},
+					success: function (data) {
+						window.location.href = data;
+					},
+					error: function (data) {
+						console.log(data);
+					}
 
-					});
-			}else if(data == "b3aca92c793ee0e9b1a9b0a5f5fc044e05140df3"){
+				});
+			} else if (data == "b3aca92c793ee0e9b1a9b0a5f5fc044e05140df3") {
 				$.ajax({
-						// administrator
-						url: base_url+"Login/redirect/b3aca92c793ee0e9b1a9b0a5f5fc044e05140df3",
-						type: "post",
-						dataType: "json",
-						data:{
-							username: $username,
-						},
-						success: function(data){
-							window.location.href = data;	
-						},
-						error: function(data){
-							console.log(data);	
-						}
+					// administrator
+					url: base_url + "Login/redirect/b3aca92c793ee0e9b1a9b0a5f5fc044e05140df3",
+					type: "post",
+					dataType: "json",
+					data: {
+						username: $username,
+					},
+					success: function (data) {
+						window.location.href = data;
+					},
+					error: function (data) {
+						console.log(data);
+					}
 
-					});
-			}else if(data == "204036a1ef6e7360e536300ea78c6aeb4a9333dd"){
+				});
+			} else if (data == "204036a1ef6e7360e536300ea78c6aeb4a9333dd") {
 				$.ajax({
-						// student
-						url: base_url+"Login/redirect/204036a1ef6e7360e536300ea78c6aeb4a9333dd",
-						type: "post",
-						dataType: "json",
-						data:{
-							username: $username,
-						},
-						success: function(data){
-							window.location.href = data;	
-						},
-						error: function(data){
-							console.log(data);	
-						}
+					// student
+					url: base_url + "Login/redirect/204036a1ef6e7360e536300ea78c6aeb4a9333dd",
+					type: "post",
+					dataType: "json",
+					data: {
+						username: $username,
+					},
+					success: function (data) {
+						window.location.href = data;
+					},
+					error: function (data) {
+						console.log(data);
+					}
 
-					});
-			}else if(data == "ea1462c1fe6251c885dce5002ad73edb0f613628"){
+				});
+			} else if (data == "ea1462c1fe6251c885dce5002ad73edb0f613628") {
 				$.ajax({
-						// student
-						url: base_url+"Login/redirect/ea1462c1fe6251c885dce5002ad73edb0f613628",
-						type: "post",
-						dataType: "json",
-						data:{
-							username: $username,
-						},
-						success: function(data){
-							window.location.href = data;	
-						},
-						error: function(data){
-							console.log(data);	
-						}
+					// student
+					url: base_url + "Login/redirect/ea1462c1fe6251c885dce5002ad73edb0f613628",
+					type: "post",
+					dataType: "json",
+					data: {
+						username: $username,
+					},
+					success: function (data) {
+						window.location.href = data;
+					},
+					error: function (data) {
+						console.log(data);
+					}
 
-					});
-			}else{ 
+				});
+			} else {
 				$("#login-username").addClass('invalid');
 				$("#login-password").addClass('invalid');
 				$("#login-message").css('display', 'block');
 				$("#login-message").html(data);
 			}
 		},
-		error: function(data){
+		error: function (data) {
 		}
 	});
 
@@ -364,31 +361,31 @@ function login_verify() {
 =            Admin JS            =
 ================================*/
 
-function initiateOnClick(){
-	showReportonClick("card-cosml","div-card-cosml","div-card-ls","div-card-lahr","div-card-lcl","div-card-clof","div-card-fic","div-card-chart","div-card-lf","div-card-mpa");
-	showReportonClick("card-ls","div-card-ls","div-card-cosml","div-card-lahr","div-card-lcl","div-card-clof","div-card-fic","div-card-chart","div-card-lf","div-card-mpa");
-	showReportonClick("card-lahr","div-card-lahr","div-card-cosml","div-card-ls","div-card-lcl","div-card-clof","div-card-fic","div-card-chart","div-card-lf","div-card-mpa");
-	showReportonClick("card-lcl","div-card-lcl","div-card-cosml","div-card-ls","div-card-lahr","div-card-clof","div-card-fic","div-card-chart","div-card-lf","div-card-mpa");
-	showReportonClick("card-clof","div-card-clof","div-card-lcl","div-card-cosml","div-card-ls","div-card-lahr","div-card-fic","div-card-chart","div-card-lf","div-card-mpa");
-	showReportonClick("card-fic","div-card-fic","div-card-lcl","div-card-cosml","div-card-ls","div-card-lahr","div-card-clof","div-card-chart","div-card-lf","div-card-mpa");
-	showReportonClick("card-chart","div-card-chart","div-card-fic","div-card-lcl","div-card-cosml","div-card-ls","div-card-lahr","div-card-clof","div-card-lf","div-card-mpa");
-	showReportonClick("card-lf","div-card-lf","div-card-fic","div-card-lcl","div-card-cosml","div-card-ls","div-card-lahr","div-card-clof","div-card-chart","div-card-mpa");
-	showReportonClick("card-mpa","div-card-mpa","div-card-fic","div-card-lcl","div-card-cosml","div-card-ls","div-card-lahr","div-card-clof","div-card-chart","div-card-lf");
+function initiateOnClick() {
+	showReportonClick("card-cosml", "div-card-cosml", "div-card-ls", "div-card-lahr", "div-card-lcl", "div-card-clof", "div-card-fic", "div-card-chart", "div-card-lf", "div-card-mpa");
+	showReportonClick("card-ls", "div-card-ls", "div-card-cosml", "div-card-lahr", "div-card-lcl", "div-card-clof", "div-card-fic", "div-card-chart", "div-card-lf", "div-card-mpa");
+	showReportonClick("card-lahr", "div-card-lahr", "div-card-cosml", "div-card-ls", "div-card-lcl", "div-card-clof", "div-card-fic", "div-card-chart", "div-card-lf", "div-card-mpa");
+	showReportonClick("card-lcl", "div-card-lcl", "div-card-cosml", "div-card-ls", "div-card-lahr", "div-card-clof", "div-card-fic", "div-card-chart", "div-card-lf", "div-card-mpa");
+	showReportonClick("card-clof", "div-card-clof", "div-card-lcl", "div-card-cosml", "div-card-ls", "div-card-lahr", "div-card-fic", "div-card-chart", "div-card-lf", "div-card-mpa");
+	showReportonClick("card-fic", "div-card-fic", "div-card-lcl", "div-card-cosml", "div-card-ls", "div-card-lahr", "div-card-clof", "div-card-chart", "div-card-lf", "div-card-mpa");
+	showReportonClick("card-chart", "div-card-chart", "div-card-fic", "div-card-lcl", "div-card-cosml", "div-card-ls", "div-card-lahr", "div-card-clof", "div-card-lf", "div-card-mpa");
+	showReportonClick("card-lf", "div-card-lf", "div-card-fic", "div-card-lcl", "div-card-cosml", "div-card-ls", "div-card-lahr", "div-card-clof", "div-card-chart", "div-card-mpa");
+	showReportonClick("card-mpa", "div-card-mpa", "div-card-fic", "div-card-lcl", "div-card-cosml", "div-card-ls", "div-card-lahr", "div-card-clof", "div-card-chart", "div-card-lf");
 
 }
 
-function showReportonClick(id,div_id,div_hide_1,div_hide_2,div_hide_3,div_hide_4,div_hide_5,div_hide_6,div_hide_7,div_hide_8){
-	$("#"+id).click(function(event) {
-		$("#"+div_id).fadeIn();
-		$("#"+div_id).css("display","block");
-		$("#"+div_hide_1).css("display","none");
-		$("#"+div_hide_2).css("display","none");
-		$("#"+div_hide_3).css("display","none");
-		$("#"+div_hide_4).css("display","none");
-		$("#"+div_hide_5).css("display","none");
-		$("#"+div_hide_6).css("display","none");
-		$("#"+div_hide_7).css("display","none");
-		$("#"+div_hide_8).css("display","none");
+function showReportonClick(id, div_id, div_hide_1, div_hide_2, div_hide_3, div_hide_4, div_hide_5, div_hide_6, div_hide_7, div_hide_8) {
+	$("#" + id).click(function (event) {
+		$("#" + div_id).fadeIn();
+		$("#" + div_id).css("display", "block");
+		$("#" + div_hide_1).css("display", "none");
+		$("#" + div_hide_2).css("display", "none");
+		$("#" + div_hide_3).css("display", "none");
+		$("#" + div_hide_4).css("display", "none");
+		$("#" + div_hide_5).css("display", "none");
+		$("#" + div_hide_6).css("display", "none");
+		$("#" + div_hide_7).css("display", "none");
+		$("#" + div_hide_8).css("display", "none");
 
 	});
 }
